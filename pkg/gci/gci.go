6a40cd1451bd3bc603f440dde7258b5497de7357
Xiang Dai <long0dai@foxmail.com>
2020-08-07 13:56:22 +0800

    Remove extra space
    Signed-off-by: Xiang Dai <long0dai@foxmail.com>

--- pkg/gci/gci.go
+++ pkg/gci/gci.go
@@ -145,7 +145,7 @@ func getPkgInfo(line string, comment bool) (string, string, string) {
 		if len(pkgArray) > 1 {
 			return pkgArray[1], pkgArray[0], fmt.Sprintf("%s%s%s", commentFlag, blank, strings.TrimSpace(s[1]))
 		} else {
-			return pkgArray[0], "", fmt.Sprintf("%s%s%s", commentFlag, blank, strings.TrimSpace(s[1]))
+			return strings.TrimSpace(pkgArray[0]), "", fmt.Sprintf("%s%s%s", commentFlag, blank, strings.TrimSpace(s[1]))
 		}
 	} else {
 		pkgArray := strings.Split(line, blank)
