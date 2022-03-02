66fd3f5b58cd90c53538b783451d39e037071bbf
Charles Korn <me@charleskorn.com>
2022-03-02 16:32:32 +1100

    Fix "Error: an error occured while trying to parse imports: path is missing starting quotes" when parsing a file with block comments.
    Fixes https://github.com/daixiang0/gci/issues/48.
    
    Note that this only handles simple cases for block comments, and does
    not cover more complex cases (see FIXME in parse.go).
    
    Signed-off-by: Charles Korn <me@charleskorn.com>

--- pkg/gci/parse.go
+++ pkg/gci/parse.go
@@ -10,25 +10,56 @@ import (
 // Recursively parses import lines into a list of ImportDefs
 func parseToImportDefinitions(unformattedLines []string) ([]importPkg.ImportDef, error) {
 	newImport := importPkg.ImportDef{}
+	inBlockComment := false
 	for index, unformattedLine := range unformattedLines {
 		line := strings.TrimSpace(unformattedLine)
 		if line == "" {
 			//empty line --> starts a new import
 			return parseToImportDefinitions(unformattedLines[index+1:])
 		}
-		if strings.HasPrefix(line, constants.CommentFlag) {
+		if strings.HasPrefix(line, constants.LineCommentFlag) {
 			// comment line
 			newImport.PrefixComment = append(newImport.PrefixComment, line)
 			continue
 		}
+
+		// FIXME: this doesn't correctly handle block comments that start part-way through a line or end part-way through a line, for example:
+		//   /* some comment */ "golang.org/x/tools"
+		// Or:
+		//  /* some comment
+		//  */ "golang.org/x/tools"
+		//
+		// It only supports block comments that start and end on their own line, for example:
+		//   /* some comment */
+		//   "golang.org/x/tools"
+		// Or:
+		//   /*
+		//     some comment
+		//   */
+		//   "golang.org/x/tools"
+		if inBlockComment {
+			if strings.HasSuffix(line, constants.BlockCommentEndFlag) {
+				inBlockComment = false
+			} else {
+				line = "\t" + line
+			}
+
+			newImport.PrefixComment = append(newImport.PrefixComment, line)
+			continue
+		} else if strings.HasPrefix(line, constants.BlockCommentStartFlag) {
+			inBlockComment = true
+			newImport.PrefixComment = append(newImport.PrefixComment, line)
+			continue
+		}
+
 		// split inline comment from import
-		importSegments := strings.SplitN(line, constants.CommentFlag, 2)
+		importSegments := strings.SplitN(line, constants.LineCommentFlag, 2)
 		switch len(importSegments) {
 		case 1:
 			// no inline comment
 		case 2:
 			// inline comment present
-			newImport.InlineComment = constants.CommentFlag + importSegments[1]
+			newImport.InlineComment = constants.LineCommentFlag + importSegments[1]
 		default:
 			return nil, InvalidImportSplitError{importSegments}
 		}
