30a50b8f9d4435eb1457912ba2d89488a89cc7a8
Marco Nenciarini <mnencia@kcore.it>
2022-07-25 02:42:31 +0200

    Support grouping dot and blank imports at the end (#88)
    * feat: Support grouping dot and blank imports at the end
    
    Signed-off-by: Marco Nenciarini <marco.nenciarini@enterprisedb.com>
    
    * doc: Document blank and dot groups
    
    Signed-off-by: Marco Nenciarini <marco.nenciarini@enterprisedb.com>

---
+++ pkg/gci/internal/testdata/dot-and-blank.out.go
@@ -0,0 +1,16 @@
+package main
+import (
+	"fmt"
+
+	g "github.com/golang"
+
+	"github.com/daixiang0/a"
+	"github.com/daixiang0/gci"
+	"github.com/daixiang0/gci/subtest"
+
+	_ "github.com/daixiang0/gci/blank"
+	_ "github.com/golang/blank"
+
+	. "github.com/daixiang0/gci/dot"
+	. "github.com/golang/dot"
+)
