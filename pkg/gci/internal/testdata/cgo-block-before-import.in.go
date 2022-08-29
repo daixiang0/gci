881424b3b096dd53e64f20417ac75e59d29f3c2e
Loong Dai <loong.dai@intel.com>
2022-08-29 14:46:33 +0800

    fix cgo issue (#104)
    Signed-off-by: Loong <loong.dai@intel.com>

---
+++ pkg/gci/internal/testdata/cgo-block-before-import.in.go
@@ -0,0 +1,15 @@
+package main
+
+// #cgo CFLAGS: -DPNG_DEBUG=1
+// #cgo amd64 386 CFLAGS: -DX86=1
+// #cgo LDFLAGS: -lpng
+// #include <png.h>
+import "C"
+
+import (
+	"fmt"
+
+	"github.com/daixiang0/gci"
+
+	g "github.com/golang"
+)
