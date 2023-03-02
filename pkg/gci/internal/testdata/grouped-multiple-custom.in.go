65b147e8805f1922051290f39d61b7a34ba76857
ccoVeille <3875889+ccoVeille@users.noreply.github.com>
2023-03-02 03:09:45 +0100

    Support multiple prefixes to group them together #107 (#142)
    This changes allows to use this to group multiple prefixes
    
    
    Signed-off-by: ccoVeille <3875889+ccoVeille@users.noreply.github.com>

---
+++ pkg/gci/internal/testdata/grouped-multiple-custom.in.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"daixiang0/lib1"
+	"fmt"
+	"github.com/daixiang0/gci"
+	"gitlab.com/daixiang0/gci"
+	g "github.com/golang"
+	"github.com/daixiang0/gci/subtest"
+)
---
+++ pkg/gci/internal/testdata/grouped-multiple-custom.out.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	g "github.com/golang"
+
+	"daixiang0/lib1"
+	"github.com/daixiang0/gci"
+	"github.com/daixiang0/gci/subtest"
+	"gitlab.com/daixiang0/gci"
+)
